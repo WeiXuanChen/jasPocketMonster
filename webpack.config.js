280a3f9f519802aad3108e7423a68cfff359b7fb
JamieChen <jamie.chen@gogoro.com>
2020-09-30 15:39:46 +0800

    Fix Babel issue.

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
       },
       {
         test: /\.s[ac]ss$/i,
